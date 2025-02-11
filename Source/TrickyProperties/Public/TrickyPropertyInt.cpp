906bf36df2d8cdb74fef34b1f6c5940ce6244f41
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-02-11 19:49:02 +0200

    Create new TrickyPropertyInt

---
+++ Source/TrickyProperties/Public/TrickyPropertyInt.cpp
@@ -0,0 +1,95 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+
+#include "TrickyPropertyInt.h"
+
+bool FTrickyPropertyInt::IncreaseMinValue(const int32 Amount)
+{
+	if (Amount <= 0.f)
+	{
+		return false;
+	}
+
+	MinValue += Amount;
+	return true;
+}
+
+bool FTrickyPropertyInt::DecreaseMinValue(const int32 Amount)
+{
+	if (Amount <= 0.f)
+	{
+		return false;
+	}
+
+	MinValue -= Amount;
+	return true;
+}
+
+bool FTrickyPropertyInt::IncreaseValue(const int32 Amount)
+{
+	if (Amount <= 0.f)
+	{
+		return false;
+	}
+
+	Value += Amount;
+	return true;
+}
+
+bool FTrickyPropertyInt::DecreaseValue(const int32 Amount)
+{
+	if (Amount <= 0.f)
+	{
+		return false;
+	}
+
+	Value -= Amount;
+	return true;
+}
+
+bool FTrickyPropertyInt::IncreaseMaxValue(const int32 Amount)
+{
+	if (Amount <= 0.f)
+	{
+		return false;
+	}
+
+	MaxValue += Amount;
+	return true;
+}
+
+bool FTrickyPropertyInt::DecreaseMaxValue(const int32 Amount)
+{
+	if (Amount <= 0.f)
+	{
+		return false;
+	}
+
+	MaxValue -= Amount;
+	return true;
+}
+
+void FTrickyPropertyInt::ClampToMin()
+{
+	Value = FMath::Max(Value, MinValue);
+}
+
+void FTrickyPropertyInt::ClampToMax()
+{
+	Value = FMath::Min(Value, MaxValue);
+}
+
+int32 FTrickyPropertyInt::GetNormalizedValue() const
+{
+	return MaxValue > 0 ? static_cast<float>(Value) / static_cast<float>(MaxValue) : 0;
+}
+
+bool FTrickyPropertyInt::ReachedMinValue() const
+{
+	return Value <= MinValue;
+}
+
+bool FTrickyPropertyInt::ReachedMaxValue() const
+{
+	return Value >= MaxValue;
+}
---
+++ Source/TrickyProperties/Public/TrickyPropertyInt.h
@@ -0,0 +1,43 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TrickyPropertyInt.generated.h"
+
+USTRUCT(Blueprintable, BlueprintType)
+struct FTrickyPropertyInt 
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=TrickyProperty)
+	int32 MinValue = 0.0f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=TrickyProperty)
+	int32 Value = 100.0f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=TrickyProperty)
+	int32 MaxValue = 100.0f;
+
+	bool IncreaseMinValue(const int32 Amount = 1.0f);
+
+	bool DecreaseMinValue(const int32 Amount = 1.0f);
+	
+	bool IncreaseValue(const int32 Amount = 1.0f);
+
+	bool DecreaseValue(const int32 Amount = 1.0f);
+
+	bool IncreaseMaxValue(const int32 Amount = 1.0f);
+	
+	bool DecreaseMaxValue(const int32 Amount = 1.0f);
+
+	void ClampToMin();
+
+	void ClampToMax();
+	
+	int32 GetNormalizedValue() const;
+
+	bool ReachedMinValue() const;
+
+	bool ReachedMaxValue() const;
+};
