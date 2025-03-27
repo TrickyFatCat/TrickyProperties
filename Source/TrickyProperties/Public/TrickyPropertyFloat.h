bc15dd176f3cc115ac7d0f1f1114bfad2efa089b
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-03-27 22:14:58 +0200

    Fix structs implementations to fix an issue with linking during editor builds

--- Source/TrickyProperties/Public/TrickyPropertyFloat.h
+++ Source/TrickyProperties/Public/TrickyPropertyFloat.h
@@ -6,7 +6,7 @@
 #include "TrickyPropertyFloat.generated.h"
 
 USTRUCT(Blueprintable, BlueprintType)
-struct FTrickyPropertyFloat 
+struct FTrickyPropertyFloat
 {
 	GENERATED_BODY()
 
@@ -15,33 +15,108 @@ struct FTrickyPropertyFloat
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=TrickyProperty)
 	float Value = 100.0f;
-	
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=TrickyProperty)
 	float MaxValue = 100.0f;
 
-	bool IncreaseMinValue(const float Amount = 1.0f);
+	bool IncreaseMinValue(const float Amount = 1.0f)
+	{
+		if (Amount <= 0.f)
+		{
+			return false;
+		}
 
-	bool DecreaseMinValue(const float Amount = 1.0f);
-	
-	bool IncreaseValue(const float Amount = 1.0f);
+		MinValue += Amount;
+		return true;
+	}
 
-	bool DecreaseValue(const float Amount = 1.0f);
+	bool DecreaseMinValue(const float Amount = 1.0f)
+	{
+		if (Amount <= 0.f)
+		{
+			return false;
+		}
 
-	bool IncreaseMaxValue(const float Amount = 1.0f);
-	
-	bool DecreaseMaxValue(const float Amount = 1.0f);
+		MinValue -= Amount;
+		return true;
+	}
 
-	void ClampToMin();
+	bool IncreaseValue(const float Amount = 1.0f)
+	{
+		if (Amount <= 0.f)
+		{
+			return false;
+		}
 
-	void ClampToMax();
+		Value += Amount;
+		return true;
+	}
 
-	void ResetToMin();
+	bool DecreaseValue(const float Amount = 1.0f)
+	{
+		if (Amount <= 0.f)
+		{
+			return false;
+		}
 
-	void ResetToMax();
-	
-	float GetNormalizedValue() const;
+		Value -= Amount;
+		return true;
+	}
 
-	bool ReachedMinValue() const;
+	bool IncreaseMaxValue(const float Amount = 1.0f)
+	{
+		if (Amount <= 0.f)
+		{
+			return false;
+		}
 
-	bool ReachedMaxValue() const;
+		MaxValue += Amount;
+		return true;
+	}
+
+	bool DecreaseMaxValue(const float Amount = 1.0f)
+	{
+		if (Amount <= 0.f)
+		{
+			return false;
+		}
+
+		MaxValue -= Amount;
+		return true;
+	}
+
+	void ClampToMin()
+	{
+		Value = FMath::Max(Value, MinValue);
+	}
+
+	void ClampToMax()
+	{
+		Value = FMath::Min(Value, MaxValue);
+	}
+
+	void ResetToMin()
+	{
+		Value = MinValue;
+	}
+
+	void ResetToMax()
+	{
+		Value = MaxValue;
+	}
+
+	float GetNormalizedValue() const
+	{
+		return MaxValue > 0.f ? Value / MaxValue : 0.f;
+	}
+
+	bool ReachedMinValue() const
+	{
+		return Value <= MinValue;
+	}
+
+	bool ReachedMaxValue() const
+	{
+		return Value >= MaxValue;
+	}
 };
--- Source/TrickyProperties/Public/TrickyPropertyInt.h
+++ Source/TrickyProperties/Public/TrickyPropertyInt.h
@@ -6,42 +6,119 @@
 #include "TrickyPropertyInt.generated.h"
 
 USTRUCT(Blueprintable, BlueprintType)
-struct FTrickyPropertyInt 
+struct FTrickyPropertyInt
 {
 	GENERATED_BODY()
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=TrickyProperty)
 	int32 MinValue = 0.0f;
-	
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=TrickyProperty)
 	int32 Value = 100.0f;
-	
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=TrickyProperty)
 	int32 MaxValue = 100.0f;
 
-	bool IncreaseMinValue(const int32 Amount = 1.0f);
+	bool IncreaseMinValue(const int32 Amount = 1.0f)
+	{
+		if (Amount <= 0.f)
+		{
+			return false;
+		}
 
-	bool DecreaseMinValue(const int32 Amount = 1.0f);
-	
-	bool IncreaseValue(const int32 Amount = 1.0f);
+		MinValue += Amount;
+		return true;
+	}
 
-	bool DecreaseValue(const int32 Amount = 1.0f);
+	bool DecreaseMinValue(const int32 Amount = 1.0f)
+	{
+		if (Amount <= 0.f)
+		{
+			return false;
+		}
 
-	bool IncreaseMaxValue(const int32 Amount = 1.0f);
-	
-	bool DecreaseMaxValue(const int32 Amount = 1.0f);
+		MinValue -= Amount;
+		return true;
+	}
 
-	void ClampToMin();
+	bool IncreaseValue(const int32 Amount = 1.0f)
+	{
+		if (Amount <= 0.f)
+		{
+			return false;
+		}
 
-	void ClampToMax();
+		Value += Amount;
+		return true;
+	}
 
-	void ResetToMin();
+	bool DecreaseValue(const int32 Amount = 1.0f)
+	{
+		if (Amount <= 0.f)
+		{
+			return false;
+		}
 
-	void ResetToMax();
-	
-	int32 GetNormalizedValue() const;
+		Value -= Amount;
+		return true;
+	}
 
-	bool ReachedMinValue() const;
+	bool IncreaseMaxValue(const int32 Amount = 1.0f)
+	{
+		if (Amount <= 0.f)
+		{
+			return false;
+		}
 
-	bool ReachedMaxValue() const;
+		MaxValue += Amount;
+		return true;
+	}
+
+
+	bool DecreaseMaxValue(const int32 Amount = 1.0f)
+	{
+		if (Amount <= 0.f)
+		{
+			return false;
+		}
+
+		MaxValue -= Amount;
+		return true;
+	}
+
+
+	void ClampToMin()
+	{
+		Value = FMath::Max(Value, MinValue);
+	}
+
+	void ClampToMax()
+	{
+		Value = FMath::Min(Value, MaxValue);
+	}
+
+	void ResetToMin()
+	{
+		Value = MinValue;
+	}
+
+	void ResetToMax()
+	{
+		Value = MaxValue;
+	}
+
+	int32 GetNormalizedValue() const
+	{
+		return MaxValue > 0 ? static_cast<float>(Value) / static_cast<float>(MaxValue) : 0;
+	}
+
+	bool ReachedMinValue() const
+	{
+		return Value <= MinValue;
+	}
+
+	bool ReachedMaxValue() const
+	{
+		return Value >= MaxValue;
+	}
 };
